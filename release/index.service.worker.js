913569553f744210459c82dab76a7099d551c497
satohashi <yenot.raketa@gmail.com>
2026-01-05 03:04:32 +0300

    Try to fix deploy again

--- release/index.service.worker.js
+++ release/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1767571298|879603646';
+const CACHE_VERSION = '1767571459|1041135100';
 /** @type {string} */
 const CACHE_PREFIX = '15 Puzzle-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
